82e07a3dac77c14435bb71833d5d938e13e90c17
Hyunghwan Shin <hwan.shin@gmail.com>
2024-01-26 02:44:26 +0000

    prevent cors error

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	experimental: {
+		serverActions: {
+			allowedForwardedHosts: ['0.0.0.0:3010'],
+			allowedOrigins: ['146.56.151.119:3010']
+		},
+	}
+};
 
 module.exports = nextConfig;
