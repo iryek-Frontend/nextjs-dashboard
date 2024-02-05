c455755472f4c8d7747b04cd162531f374c1bd82
Hyunghwan Shin <hwan.shin@gmail.com>
2024-02-05 05:15:51 +0000

    chap.16 Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard with templates',
+    default: 'Acme Dashboard',
+  },
+  description: 'Learning NextJS 14 - The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 export default function RootLayout({children,}: {children: React.ReactNode;}) {
   return (
