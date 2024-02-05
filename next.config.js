58e54de2e34379e2ebddfa33caa764a5d7564de0
Hyunghwan Shin <hwan.shin@gmail.com>
2024-02-05 02:01:30 +0000

    finish chap.15 authentication

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
 	experimental: {
 		serverActions: {
 			allowedForwardedHosts: ['0.0.0.0:3010'],
-			allowedOrigins: ['146.56.151.119:3010']
+			allowedOrigins: ['146.56.151.119:3010', 'localhost:3010']
 		},
 	}
 };
